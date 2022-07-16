7db226ce7b98bd087d452e7c6de85de872f2f499
unknown <thomas.warin.pro@gmail.com>
2022-07-16 14:34:44 +0200

    exo 3 fonctionnel

--- js/exercice3.js
+++ js/exercice3.js
@@ -1,49 +1,64 @@
-//
-fetch("http://universities.hipolabs.com/search?country=Andorra")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    renderUnivercities(data);
-  })
-  .catch((error) => {
-    console.log(`Error: `, error);
-  });
+// Fonction anonyme
+(function () {
 
-function renderUnivercities(data) {
-  const section = createDOMElement("section", "", document.body, [
-    {
-      name: "style",
-      value: "display: flex; justify-content: space-evenly; flex-wrap: wrap;",
-    },
-  ]);
-  data.array.forEach((univercity) => {
-    const article = createDOMElement("article", "", section, [
+  const select = document.getElementById("country_select");
+  const link = "http://universities.hipolabs.com/search?country=";
+
+  updateFetch();
+
+  select.addEventListener("input", updateFetch);
+
+  function updateFetch() {
+    const country = select.selectedOptions[0].value;
+    fetch(link + country)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        renderUnivercities(data);
+      })
+      .catch((error) => {
+        console.log(`Error: `, error);
+      });
+  }
+
+  function renderUnivercities(data) {
+    let section = document.querySelector("section");
+    if(section != null) section.remove();
+    section = createDOMElement("section", "", document.body, [
       {
         name: "style",
-        value:
-          "border: 1px solid blue; padding: 20px; margin: 30px 0; flex-basis: 28%;",
+        value: "display: flex; justify-content: space-evenly; flex-wrap: wrap;",
       },
     ]);
-    const title = createDOMElement("h2", univercity.name, article);
-  });
-}
+    data.forEach((univercity) => {
+      const article = createDOMElement("article", "", section, [
+        {
+          name: "style",
+          value:
+            "border: 1px solid blue; padding: 20px; margin: 30px 0; flex-basis: 28%;",
+        },
+      ]);
+      const title = createDOMElement("h2", univercity.name, article);
+    });
+  }
 
-/**
- *  créer un élément dans le dom à l'emplacement voulue
- * @param {String} balise
- * @param {String} text
- * @param {DomElement} parent
- * @param {Array} tab
- * @return {DomElement} l'élement créé
- */
-function createDOMElement(balise, text, parent, tab = []) {
-  const element = document.createElement(balise);
-  element.textContent = text;
-  parent.appendChild(element);
-  tab.forEach((attribute) => {
-    if (attribute.name && attribute.value)
-      element.setAttribute(attribute.name, attribute.value);
-  });
-  return element;
-}
+  /**
+   *  créer un élément dans le dom à l'emplacement voulue
+   * @param {String} balise
+   * @param {String} text
+   * @param {DomElement} parent
+   * @param {Array} tab
+   * @return {DomElement} l'élement créé
+   */
+  function createDOMElement(balise, text, parent, tab = []) {
+    const element = document.createElement(balise);
+    element.textContent = text;
+    parent.appendChild(element);
+    tab.forEach((attribute) => {
+      if (attribute.name && attribute.value)
+        element.setAttribute(attribute.name, attribute.value);
+    });
+    return element;
+  }
+})();
